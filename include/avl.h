55f6d6a5302c189f68cf1a0a2ea4160383151e12
DaQshin <dakshin.csml@gmail.com>
2026-05-04 14:35:00 +0000

    feat: implement header file for AVL tree

---
+++ include/avl.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct AVLNode {
+    struct AVLNode* parent = nullptr;
+    struct AVLNode* left = nullptr;
+    struct AVLNode* right = nullptr;
+    uint32_t height = 0;
+    uint32_t count = 0;
+};
+
+inline void avl_init(AVLNode* node){
+    node->parent = node->left = node->right = nullptr;
+    node->height = 1;
+    node->count = 1;
+}
+
+inline uint32_t avl_height(AVLNode* node){ return node ? node->height : 0;}
+inline uint32_t avl_count(AVLNode* node){return node ? node->count : 0;}
+
+AVLNode* avl_fix(AVLNode* node);
+AVLNode* avl_del(AVLNode* node);
